ffc22ab41fe7833cd53226480fa82ca7cdd06d3a
Anh Tuan <thongocnguyen1011@gmail.com>
2024-11-16 08:56:40 +0700

    toggle theme test

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -62,7 +62,7 @@ function ThemeToggle() {
       style={{
         height: "100vh",
       }}
-      className={"theme-container"}
+      className="theme-container"
       onDoubleClick={toggleDark}
     ></div>
   );
